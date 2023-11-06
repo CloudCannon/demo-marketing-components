809cdb35b604f27d8a61db6fda5279d41c5a6df0
Zach Leatherman <zach@cloudcannon.com>
2023-11-06 14:59:00 -0600

    More styles

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,9 @@
 module.exports = function(eleventyConfig) {
+  // Global Layout
+  eleventyConfig.addGlobalData("layout", "marketing-components/base.liquid");
+
+  // Assets
   eleventyConfig.addPassthroughCopy({
     "_includes/marketing-components/public/": "/"
-  })
+  });
 };
